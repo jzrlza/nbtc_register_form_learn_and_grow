24f6378ffb8cd7b56cc19d8992c51fc3a21f34d8
jzrlza <parinvut12345@gmail.com>
2026-01-12 06:49:56 +0700

    .

--- register_form_learn_and_grow_database_production/testuserdb.sql
+++ register_form_learn_and_grow_database_production/testuserdb.sql
@@ -7,8 +7,9 @@ CREATE TABLE `users`  (
   `two_factor_secret` varchar(255) NULL DEFAULT NULL,
   `is_2fa_enabled` tinyint(1) DEFAULT 0,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `is_deleted` tinyint(1) DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 76 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 # password = nbtctest
-INSERT INTO `users` VALUES (1, 'one_admin', '$2a$12$2XXyaujvn6FBac/zLLib0OavSp8yGCtpxbNFcORzvzIm0UKYQq5o.', 1, NULL, 0, NOW());
+INSERT INTO `users` VALUES (1, 'one_admin', '$2a$12$2XXyaujvn6FBac/zLLib0OavSp8yGCtpxbNFcORzvzIm0UKYQq5o.', 1, NULL, 0, NOW(), 0);
