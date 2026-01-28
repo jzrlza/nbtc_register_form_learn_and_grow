6dd4af8930d93996bc2f71fbaceab6e21e363c6a
jzrlza <parinvut12345@gmail.com>
2026-01-28 21:45:10 +0700

    .

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout ssl/private.key \
-  -out ssl/certificate.crt \
-  -subj "/C=US/ST=State/L=City/O=Organization/CN=localhost"
-docker-compose up -d --build
-echo "Application is starting... Check logs with: docker-compose logs -f"
