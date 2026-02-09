fd957954fe85586e908f6baaba324e69679e06bf
jzrlza <parinvut12345@gmail.com>
2026-02-09 18:43:16 +0700

    .

--- register_form_learn_and_grow_database_production/self.sql
+++ register_form_learn_and_grow_database_production/self.sql
@@ -4,7 +4,7 @@ SELECT id, emp_name FROM employee WHERE dept_id = 7 OR dept_id = 18;
 
 
 UPDATE users SET is_2fa_enabled = 1, two_factor_secret = NULL WHERE employee_id = x;
-
+UPDATE users SET username = '???' WHERE id = x;
 
 INSERT INTO users (employee_id, is_2fa_enabled, is_deleted) VALUES (x, 1, 0);
 
