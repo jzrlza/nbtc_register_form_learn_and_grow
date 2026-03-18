9e39d16e1637ac6685dfd72390b93057679dd5bb
jzrlza <parinvut12345@gmail.com>
2026-03-18 20:37:04 +0700

    .

--- register_form_learn_and_grow_database_production/migrate04.sql
+++ register_form_learn_and_grow_database_production/migrate04.sql
@@ -3,3 +3,6 @@ DROP COLUMN `is_register_one`;
 
 ALTER TABLE `employee`
 DROP COLUMN `is_register`;
+
+
+#INSERT INTO `register_one` (emp_id, phone_number, is_attend, take_van_id, van_round_id, take_food) VALUES (random_int_from_1_to_2077, random_string_as_tel_number, 0_or_1, 1_to_4 [or null if is_attend = 0], 1_to_3 [or null if is_attend = 0 or take_van_id = 3 to 4 ], 1_to_2);
